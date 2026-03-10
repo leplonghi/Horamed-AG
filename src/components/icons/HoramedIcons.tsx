27e67e0b5e23ae824b0cefee9ec1dbb9d30c94aa
Luis Eduardo Longhi <leplonghi@gmail.com>
2026-03-10 18:58:46 -0300

    ewdewdewd

--- src/components/icons/HoramedIcons.tsx
+++ src/components/icons/HoramedIcons.tsx
@@ -399,6 +399,14 @@ export const IconDownload = (props: IconProps) => (
     </BaseIcon>
 );
 
+export const IconUpload = (props: IconProps) => (
+    <BaseIcon {...props}>
+        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" strokeOpacity="0.3" />
+        <polyline points="17 8 12 3 7 8" />
+        <line x1="12" y1="3" x2="12" y2="15" />
+    </BaseIcon>
+);
+
 export const IconExternalLink = (props: IconProps) => (
     <BaseIcon {...props}>
         <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" strokeOpacity="0.3" />
